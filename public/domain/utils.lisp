eec209dd7818ac8fb4b90f1a0b638a396e4248f5
Adlai Chandrasekhar <adlai.chandrasekhar@gmail.com>
2024-09-08 06:12:56 +0300

    PLEASE HUMAN PLEASE FOLWOL PLOX DRUG

--- public/domain/utils.lisp
+++ public/domain/utils.lisp
@@ -1,3 +1,29 @@
 (cl:in-package #:towel.public-domain-utils)
 
-(cerror "quit, please")
+;;; (cerror "quit, please")(in-package #:scalpl.net)
+
+;;; poached out of cjhunt; useful for debugging the sewage flooded by exchanges
+;;; it only works for the alist representation, because that is least ambiguous
+
+(defun pprint-json (*standard-output* json)
+  (typecase json ; see cl-json's source, or at least util.lisp, for the format
+    (complex (error "Congratulations, you are wasting your own time."))
+    (real (format t "~A" json))  ; TODO: error instead of exceeding JS precision
+    (list (pprint-logical-block (*standard-output* json :prefix "{" :suffix "}")
+            (loop for (key . value) = (pop json) while key do
+                 (pprint-logical-block (*standard-output* ())
+                   (format t "\"~(~A~)\": ~@_" key)
+                   (pprint-json *standard-output* value))
+                 (when json (format t ", ") (pprint-newline :fill)))))
+    (string (format t "~S" json))
+    (vector (pprint-logical-block (nil nil :prefix "[" :suffix "]")
+              (let ((end (length json)) (i 0))
+                (when (plusp end)
+                  (loop
+                     (pprint-json *standard-output* (aref json i))
+                     (if (= (incf i) end) (return nil))
+                     (format t ", ")
+                     (pprint-newline :fill))))))
+    (t (format t "~(~A~)" json))))
+
+;;; CINGULAR
